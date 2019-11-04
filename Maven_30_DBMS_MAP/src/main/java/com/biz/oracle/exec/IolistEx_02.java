5caf8ec9f72011e68b159fda7a18fdc05e280986
callor <callor@callor.com>
2019-11-04 10:25:38 +0900

    2019-11-04

---
+++ Maven_30_DBMS_MAP/src/main/java/com/biz/oracle/exec/IolistEx_02.java
@@ -0,0 +1,15 @@
+package com.biz.oracle.exec;
+
+import com.biz.oracle.service.IolistServiceV1;
+
+public class IolistEx_02 {
+
+	public static void main(String[] args) {
+
+		IolistServiceV1 is = new IolistServiceV1();
+		is.selectMapListView();
+		
+		
+	}
+
+}
