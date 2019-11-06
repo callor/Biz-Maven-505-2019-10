9a4de10b46cead05a8b75882cc3c8f57f6d8012f
callor <callor@callor.com>
2019-11-06 14:38:45 +0900

    2019-11-06

--- Maven_40_DBMS_SEC/src/main/java/com/biz/sec/mapper/MDDao.java
+++ Maven_40_DBMS_SEC/src/main/java/com/biz/sec/mapper/MDDao.java
@@ -2,10 +2,12 @@ package com.biz.sec.mapper;
 
 import java.util.List;
 
+import com.biz.sec.persistence.MasterDTO;
 import com.biz.sec.persistence.MasterDetailVO;
 
 public interface MDDao {
 	
 	public List<MasterDetailVO> selectAll();
+	public int insertMaster(MasterDTO masterDTO);
 
 }
