b3b067a83e99914fcce6a453e516574cee39fdf2
callor <callor@callor.com>
2019-11-07 13:33:01 +0900

    2019-11-07

--- Maven_40_DBMS_CBT_01/src/main/java/com/biz/sec/config/DBConnection.java
+++ Maven_40_DBMS_CBT_01/src/main/java/com/biz/sec/config/DBConnection.java
@@ -2,36 +2,19 @@ package com.biz.sec.config;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Map;
 import java.util.Properties;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
-public class DBConnectionSEC {
+public class DBConnection {
 
 	private static SqlSessionFactory sqlSessionFactory = null;
+	
 	static {
-		
-		// 전체 환경변수 읽어오기
-		Map<String,String> envList = System.getenv();
-		
-		// 전체 환경변수중 BIZ 키 변수값 추출
-		String saltPass = System.getenv().get("BIZ");
-		
-		// 환경변수중 BIZ 키값만 가져오기
-		saltPass = System.getenv("BIZ");
-		
 		String configFile = "com/biz/sec/config/mybatis-config.xml";
 		InputStream is = null;
-		StandardPBEStringEncryptor pbEnc = new StandardPBEStringEncryptor();
-		pbEnc.setAlgorithm("PBEWithMD5AndDES");
-		pbEnc.setPassword(saltPass);
-		
-		String userName = "7fdHKwBMqCaXo6BrKPzUxA==";
-		String password = "l4lnLOlYvnuL7fNc0B3evQ==";
 		
 		/*
 		 * 
@@ -41,8 +24,13 @@ public class DBConnectionSEC {
 		 * 
 		 */
 		Properties pros = new Properties();
-		pros.put("username",pbEnc.decrypt(userName));
-		pros.put("password",pbEnc.decrypt(password));
+		
+		/*
+		 * DBMS에 접근할 username과 passowrd를 실제 값으로 적용하고
+		 * 프로젝트를 실행할 것
+		 */
+		pros.put("username","USERNAME");
+		pros.put("password","PASSWORD");
 		
 		try {
 			is = Resources.getResourceAsStream(configFile);
